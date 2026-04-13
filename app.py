07053e2c6b4969746c73faaa92b2cb8e77b3f7e9
emilyvgr <emilyvalentinagallardor@gmail.com>
2026-04-12 20:02:36 -0400

    dasboard5

--- app.py
+++ app.py
@@ -2,30 +2,68 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.express as px
-from PIL import Image
 
-st.set_page_config(page_title="Proyecto Cultivos", page_icon="logo solo cultivo science sin fondo.png",layout="wide")
+st.set_page_config(page_title="Proyecto Cultivos", page_icon="logo solo.svg", layout="wide")
 
-logo = Image.open("logo cultivo science sin fondo.png") 
+st.markdown("""
+    <style>
+    /* 1. CAMBIAR COLOR DE LA BARRA LATERAL */
+    section[data-testid="stSidebar"] {
+        background-color: #E8F5E9; /* Verde pastel suave */
+    }
 
-st.sidebar.image(logo, use_container_width=True)
+    /* 2. CAMBIAR COLOR DE LA BARRA SUPERIOR (Header) */
+    header[data-testid="stHeader"] {
+        background-color: #F1F8E9;/
+        color: white;
+    }
 
-col1, col2, col3 = st.columns([0.5, 3.5, 0.5])
+    /* 3. CAMBIAR COLOR DE LOS WIDGETS (Botones, selectores, etc.) */
+    /* Cambia el color de fondo de los inputs y selectores */
+    .stSelectbox div[data-baseweb="select"], .stTextInput div[data-baseweb="input"] {
+        background-color: #0288D1;
+        border-radius: 10px;
+    }
 
-with col1:
-    st.image("logo cultivo science sin fondo.png", width=150)
+    /* Cambia el color de los botones */
+    div.stButton > button {
+        background-color: #FBC02D;
+        color: #444444;
+        border-radius: 20px;
+        border: none;
+        transition: 0.3s;
+    }
+    
+    /* Efecto hover para los botones */
+    div.stButton > button:hover {
+        background-color: #AEC6CF; /* Cambia a azul pastel al pasar el mouse */
+        color: white;
+    }
 
-with col2:
+    /* 4. FONDO DE LA PÁGINA PRINCIPAL */
+    .stApp {
+        background-color: #F1F8E9;/
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
+colA, colB, colC = st.columns([0.2, 3, 0.2])
+with colA:
+    st.image("logo solo.svg", width=150)
+with colB:
    st.markdown("""
-    <h1 style='text-align: center; color: #000000; font-size: 45px; font-family: Arial Black;'>
+    <h1 style='text-align: center; color: #000000; font-size: 30px; font-family: Arial Black;'>
         Análisis de los Factores Determinantes en Ciclos Agrícola
     </h1>
     """, unsafe_allow_html=True)
 st.divider()
 
+col1, col2, col3 = st.sidebar.columns([1, 2, 1])
+with col2:
+    st.image("logo solo.svg", width=100)
 opcion = st.sidebar.selectbox(
-    "¿Qué deseas ver?",
-    ("", "Calculos estadisticos", "Frecuencia de Cultivos", "Fertilizante", "Pesticida", "Rendimiento", "Precipitación", "Fertilizante vs Precipitación", "Precipitación vs Rendimiento")
+    "Selecciona una opción",
+    ("Calculos Estadisticos", "Tabla de Datos", "Frecuencia de Cultivos", "Fertilizante", "Pesticida", "Rendimiento", "Precipitación", "Fertilizante vs Precipitación", "Precipitación vs Rendimiento")
 )
 
 df = pd.read_csv("./27. Agricultura.csv")
@@ -68,31 +106,16 @@ df['season'] = df['season'].replace('Summer', 'Zaid')
 df['season'] = df['season'].replace('Winter', 'Rabi')
 df['season'] = df['season'].replace('Autumn', 'Kharif')
 
-if opcion == "":
-    st.markdown("""
-    <style>
-    .bienvenida {
-        font-family: 'Arial black';
-        color: #000000; /* Azul Pastel */
-        font-size: 12px;
-        text-align: center;
-        font-weight: bold;
-        padding: 15px;
-        border-radius: 5px;
-        background-color: #91be8c; /* Fondo muy suave */
-    }
-    </style>
-    <p class="bienvenida">
-        Bienvenido. Por favor, selecciona una categoría en el menú lateral para comenzar.
-    </p>
-    """, unsafe_allow_html=True)
-    col1, col2, col3, col4 = st.columns([3, 2, 2, 1])
+
+
+if opcion == "Calculos Estadisticos":
+    col1, col2, col3, col4 = st.columns([2, 2, 2, 1])
     with col1:
         st.markdown("""
-    <h1 style='text-align: center; color: #444444; font-size: 15px; font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;'>
+        <h1 style='text-align: center; color: #444444; font-size: 18px; font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;'>
         Influencia de los Factores Determinantes en Ciclos Agrícolas
-    </h1>
-    """, unsafe_allow_html=True)
+        </h1>
+        """, unsafe_allow_html=True)
         st.markdown("""
 <style>
     .metric-card {
@@ -106,54 +129,91 @@ if opcion == "":
     }
     .metric-label {
         color: #555555;
-        font-size: 15px;
+        font-size: 10px;
         font-weight: bold;
         margin-bottom: 5px;
     }
     .metric-value {
         color: #AEC6CF; /* Color pastel (puedes cambiarlo por cada col) */
-        font-size: 15px
+        font-size: 10px
         font-weight: bold;
     }
 </style>
 """, unsafe_allow_html=True)
         promedios = df[['fertilizer', 'pesticide', 'yield']].mean()
-        col1, col2, col3 = st.columns(3)
-        with col1:
+        col11, col12, col13 = st.columns(3)
+        with col11:
             st.markdown(f"""
          <div class="metric-card" style="border-color: #AEC6CF;">
             <div class="metric-label">Promedio Fertilizante</div>
             <div class="metric-value" style="color: #AEC6CF;">{promedios['fertilizer']:.2f} kg</div>
-        </div>
-    """, unsafe_allow_html=True)
-
-        with col2:
+            </div>
+            """, unsafe_allow_html=True)
+        with col12:
             st.markdown(f"""
-        <div class="metric-card" style="border-color: #FFD1DC;">
+            <div class="metric-card" style="border-color: #FFD1DC;">
             <div class="metric-label">Promedio Pesticida</div>
             <div class="metric-value" style="color: #FFD1DC;">{promedios['pesticide']:.2f} L</div>
-        </div>
-    """, unsafe_allow_html=True)
-
-        with col3:
+            </div>
+            """, unsafe_allow_html=True)
+        with col13:
             st.markdown(f"""
-        <div class="metric-card" style="border-color: #B3E5BE;">
+            <div class="metric-card" style="border-color: #B3E5BE;">
             <div class="metric-label">Rendimiento Promedio</div>
             <div class="metric-value" style="color: #B3E5BE;">{promedios['yield']:.2f} Ton</div>
-        </div>
-    """, unsafe_allow_html=True)
-   
-#diseño primera pagina
+            </div>
+            """, unsafe_allow_html=True)
     with col2:
-        pass
+        st.markdown("""
+        <h1 style='text-align: center; color: #444444; font-size: 18px; font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;'>
+        Necesidad de Agroquímicos en la Temporada
+        </h1>
+        """, unsafe_allow_html=True)
+        st.markdown("""
+        <style>
+        /* Buscamos el contenedor donde Streamlit mete los dataframes */
+        [data-testid="stDataFrame"] {
+            background-color: #FFFFFF;
+            border-radius: 20px;
+            padding: 20px;
+            border: 2px solid #E0E0E0;
+            box-shadow: 2px 2px 15px rgba(0,0,0,0.05);
+        }
+        </style>
+        """, unsafe_allow_html=True)
+        matriz_corr = df[['fertilizer', 'pesticide', 'yield']].corr()
+        st.dataframe(
+        matriz_corr.style.background_gradient().format("{:.2f}"),
+        use_container_width=True
+        )
+
+
     with col3:
         pass
     with col4:
         pass
 
-elif opcion == "Calculos estadisticos":
-    st.subheader("Calculos estadisticos de los Factores Determinantes en Ciclos Agrícolas")   
-    st.dataframe(df.describe(), use_container_width=True)
+elif opcion == "Tabla de Datos":
+   st.markdown("""
+        <h1 style='text-align: center; color: #444444; font-size: 18px; font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;'>
+        Tabla de Calculos estadisticos para los Factores Determinantes en Ciclos Agrícola
+        </h1>
+        """, unsafe_allow_html=True)   
+   st.markdown("""
+        <style>
+        /* Aplicamos el diseño a los contenedores de tablas */
+        [data-testid="stDataFrame"] {
+        background-color: #FFFFFF;
+        border-radius: 20px;
+        padding: 20px;
+        border: 2px solid #E0E0E0;
+        box-shadow: 2px 2px 15px rgba(0,0,0,0.05);
+        }
+        </style>
+        """, unsafe_allow_html=True)
+   st.dataframe(
+        df.describe().style.format("{:.2f}"), 
+        use_container_width=True )
 
 elif opcion == "Frecuencia de Cultivos":
     st.subheader("Frecuencia de los Cultivos por Temporada")
@@ -185,28 +245,28 @@ elif opcion == ("Pesticida"):
     st.plotly_chart(fig4)
 
 elif opcion == ("Rendimiento"):
-    st.subheader("Gráfico de Torta: Rendimiento por Temporada")
-    yield_by_season = df.groupby('season')['yield'].mean().reset_index()
-    colors = ["#9378E7", "#52A1F7", '#00CC96'] 
-    fig5 = px.pie(yield_by_season, values='yield', names='season', color_discrete_sequence=colors) 
-    fig5.update_layout(font_color="black",  font_family="Arial Black", font_size=16)
-    st.plotly_chart(fig5)
-
-# Scatter plots: Fertilizante vs Rendimiento por Temporada
-    st.subheader("Scatter Plot: Fertilizante vs Rendimiento por Temporada")
-    colors = ["#9378E7", "#52A1F7", '#00CC96']
-    fig6 = px.scatter(df, x='fertilizer', y='yield', color='season',
+   col1, col2, col3 = st.columns([2, 2, 2])
+   with col1:
+        st.subheader("Gráfico de Torta: Rendimiento por Temporada")
+        yield_by_season = df.groupby('season')['yield'].mean().reset_index()
+        colors = ["#9378E7", "#52A1F7", '#00CC96'] 
+        fig5 = px.pie(yield_by_season, values='yield', names='season', color_discrete_sequence=colors) 
+        fig5.update_layout(font_color="black",  font_family="Arial Black", font_size=16)
+        st.plotly_chart(fig5)
+   with col2:
+        st.subheader("Scatter Plot: Fertilizante vs Rendimiento por Temporada")
+        colors = ["#9378E7", "#52A1F7", '#00CC96']
+        fig6 = px.scatter(df, x='fertilizer', y='yield', color='season',
                   labels={'fertilizer': 'Fertilizante', 'yield': 'Rendimiento', 'season': 'Temporada'}, color_discrete_sequence=colors)
-    fig6.update_layout(font_color="black",  font_family="Arial Black", font_size=16)
-    st.plotly_chart(fig6)
-
-# Scatter plots: Pesticida vs Rendimiento por Temporada
-    st.subheader("Scatter Plot: Pesticida vs Rendimiento por Temporada")
-    colors = ["#9378E7", "#52A1F7", '#00CC96']
-    fig7 = px.scatter(df, x='pesticide', y='yield', color='season',
+        fig6.update_layout(font_color="black",  font_family="Arial Black", font_size=16)
+        st.plotly_chart(fig6)
+   with col3:
+        st.subheader("Scatter Plot: Pesticida vs Rendimiento por Temporada")
+        colors = ["#9378E7", "#52A1F7", '#00CC96']
+        fig7 = px.scatter(df, x='pesticide', y='yield', color='season',
                   labels={'pesticide': 'Pesticida', 'yield': 'Rendimiento', 'season': 'Temporada'}, color_discrete_sequence=colors)
-    fig7.update_layout(font_color="black",  font_family="Arial Black", font_size=16)
-    st.plotly_chart(fig7)
+        fig7.update_layout(font_color="black",  font_family="Arial Black", font_size=16)
+        st.plotly_chart(fig7)
 
 elif opcion == ("Precipitación"):
     st.subheader("Precipitación por Temporada y por Años")
